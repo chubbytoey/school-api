04f013aafd5a4a9e338299f2d7ea19f8dbf55d38
chubbytoey <toey.sasikrang@gmail.com>
2020-08-30 23:32:04 +0700

    add GroupController

--- app/Controllers/Http/GroupController.js
+++ app/Controllers/Http/GroupController.js
@@ -1,17 +1,44 @@
 'use strict'
 
+const { group } = require("@adonisjs/framework/src/Route/Manager")
+
 const Database = use('Database')
 
 function numberTypeParamValidator(number) {
-    if(Number.isNaN(parseInt.number)) {
-        return {error:500}
-    }
+    if(Number.isNaN(parseInt.number)) 
+        return {status:200 , error: undefined , data:group}
+    return {}
 }
 
 class GroupController {
     async index() {
         const groups = await Database.table('groups')
         return groups
+    }
+    async show({request}) {
+        const {id} = request.params
+
+        const ValidatedValue = numberTypeParamValidator(id)
+
+        if(ValidatedValue.error)
+            return {status:500 , error: ValidatedValue.error , data:undefined}
+
+        const group = await Database
+            .select('*')
+            .from('groups')
+            .where('group_id',id)
+            .first()
+
+        return {status:200 , error: undefined , data: group || {}}
+    }
+    async store({request}) {
+        const {name} = request.body
+
+        if(!name) 
+        return {status:422 , error: `name is missing` , data:undefined}
+
+        const group = await Database.table('groups').insert({name})
+        return {status:200 , error:undefined , data:name}
     }
 }
 
